cd51c9c40b3abbbef75c0e391c14adca9ab5d3c3
Abilash1503 <115686328+Abilash1503@users.noreply.github.com>
2022-11-16 09:49:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const hell = document.querySelector(".timer");
+
+
+setTimeout( () =>{
+    hell.innerText = "10";
+setTimeout( () =>{
+hell.innerText = "9";
+setTimeout( ()=> {
+
+hell.innerText = "8";
+ setTimeout( ()=> {
+
+hell.innerText = "7";
+setTimeout(()=> {
+
+ hell.innerText = "6";
+setTimeout( ()=> {
+
+hell.innerText = "5";
+ setTimeout( ()=> {
+
+ hell.innerText = "4";
+ setTimeout( ()=> {
+
+     hell.innerText = "3";
+ setTimeout( () =>{
+
+  hell.innerText = "2";
+ setTimeout( () =>{
+
+hell.innerText = "1";
+ setTimeout( ()=> {
+
+hell.innerText = "Happy Independence Day!";
+
+  }, 1000);
+
+
+         }, 1000);
+
+
+  }, 1000);
+
+
+     }, 1000);
+
+      }, 1000);
+
+     }, 1000);
+
+  }, 1000);
+
+  }, 1000);
+
+        }, 1000);
+    }, 1000);
+}, 1000);
